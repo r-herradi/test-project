756c3a9e15ed01bbf23127b7708dc6bda22b7b8a
Reda Herradi <Reda.Herradi@nestle.com>
2022-08-06 22:50:00 +0100

    first commit

---
+++ pages/AboutUs.vue
@@ -0,0 +1,12 @@
+<template>
+  <p>cùest ici</p>
+</template>
+
+<script>
+import { defineComponent } from "@vue/composition-api";
+
+export default defineComponent({
+  name: "AboutUs",
+  setup() {},
+});
+</script>
